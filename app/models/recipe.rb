4363c3fc48b08e4042da8439f783af94e4fc5ebf
Tom Hallett <tomhallett@gmail.com>
2012-10-01 21:44:03 -0700

    fixing the .sh script to have pivotal_workstation:: preface.  other minor fixes:
    * updating the .sh script to use the new soloist install script
    * sorting by recipe name in .sh script and soloist_script form

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,6 +8,7 @@ class Recipe < ActiveRecord::Base
   validates :recipe_group_id, :presence => true
 
   scope :with_checked_by_default, where(:checked_by_default => true)
+  scope :by_name, order("name ASC")
 
   def github_url
     "https://github.com/pivotal/pivotal_workstation/blob/master/recipes/#{self.name}.rb"
