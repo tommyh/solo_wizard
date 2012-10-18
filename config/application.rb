034466e783aa528dcf8429a06c19e099d4b4cedc
Tom Hallett <tomhallett@gmail.com>
2012-10-17 23:17:06 -0700

    debugging heroku's failed asset precompile.  their docs say this should fix it

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,9 @@ module SoloWizard
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # preventing the app from initializing and connecting to the database
+    #  Gotten from: https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar
+    config.assets.initialize_on_precompile = false
   end
 end
